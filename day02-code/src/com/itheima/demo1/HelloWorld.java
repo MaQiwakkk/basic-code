92e0b4c825312dfcb3490d23cea5a194155e894d
王大可 <wangduck@wangdakedeMacBook-Air.local>
2026-03-02 23:59:45 +0800

    feat: finish day20 code on mac

--- day02-code/src/com/itheima/demo1/HelloWorld.java
+++ day02-code/src/com/itheima/demo1/HelloWorld.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello W你orld");
+        System.out.println("Hello World啊");
         Scanner sc = new Scanner(System.in);
     }
 }
---
+++ day20-code/src/com/itzzq/a01datedemo01/A04_SimpleDateFormat2.java
@@ -0,0 +1,21 @@
+package com.itzzq.a01datedemo01;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class A04_SimpleDateFormat2 {
+    static void main(String[] args) throws ParseException {
+        //原格式
+        String str ="2000-11-11";
+
+        //解析 SimpleDateFormat的parse方法将会识别传入的字符串得到一个Date类型对象
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf1.parse(str);
+
+        //格式化 SimpleDateFormat的format方法将date对象变为一个（之前指定过格式的）字符串对象，方便查看
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy年MM月dd日");
+        String result = sdf2.format(date);
+        System.out.println(result);
+    }
+}
